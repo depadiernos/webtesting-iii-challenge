c9abf9ede54cdffc62afb98907f140e48c76ddc7
Dennis Padiernos <depadiernos@gmail.com>
2019-10-30 19:27:42 -0500

    implement controls tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,38 @@
-// Test away!
+import React from "react";
+import * as rtl from "react-testing-library";
+import "jest-dom/extend-expect";
+
+import Controls from "./Controls";
+
+afterEach(rtl.cleanup);
+
+describe("the Controls component", () => {
+  it("shows Lock Gate button", () => {
+    const wrapper = rtl.render(<Controls />);
+    const button = wrapper.getByText(/Lock Gate/i);
+    expect(button).toExist;
+  });
+  it("shows Close Gate button", async () => {
+    const wrapper = rtl.render(<Controls />);
+    const button = wrapper.getByText(/Close Gate/i);
+    expect(button).toExist;
+  });
+  it("trigger toggleLocked function on click", () => {
+    const toggleLocked = jest.fn();
+    const wrapper = rtl.render(<Controls toggleLocked={toggleLocked} />);
+    const button = wrapper.getByText(/Lock Gate/i);
+    rtl.act(() => {
+      rtl.fireEvent.click(button);
+    });
+    expect(toggleLocked).toHaveBeenCalled;
+  });
+  it("trigger toggleClosed function on click", () => {
+    const toggleClosed = jest.fn();
+    const wrapper = rtl.render(<Controls toggleClosed={toggleClosed} />);
+    const button = wrapper.getByText(/Close Gate/i);
+    rtl.act(() => {
+      rtl.fireEvent.click(button);
+    });
+    expect(toggleClosed).toHaveBeenCalled;
+  });
+});
